684796ed6efd19a19101a7f220d3f37c4f052b8a
Shailesh Aanand <anaandshailu@gmail.com>
2022-01-15 19:24:04 +0530

    Complete Task

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from tasks.views import (
+    all_tasks_view,
+    tasks_view,
+    delete_task_view,
+    complete_task_view,
+    completed_tasks_view,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("add-task", tasks_view),
+    path("tasks", all_tasks_view),
+    path("delete-task/<int:index>", delete_task_view),
+    path("complete_task/<int:index>", complete_task_view),
+    path("completed_tasks", completed_tasks_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,35 @@
-# Add your Views Here
+from django.shortcuts import render, redirect
+from tasks.models import Task
+
+
+def tasks_view(request):
+    task = request.GET["task"]
+    Task.objects.create(title=task)
+    return redirect("/tasks")
+
+
+def completed_tasks_view(request):
+
+    return render(
+        request,
+        "completed_tasks.html",
+        {"tasks": Task.objects.filter(completed=True, deleted=False)},
+    )
+
+
+def all_tasks_view(request):
+    return render(
+        request,
+        "tasks.html",
+        {"tasks": Task.objects.filter(completed=False, deleted=False)},
+    )
+
+
+def delete_task_view(request, index):
+    Task.objects.filter(id=index).update(deleted=True)
+    return redirect("/tasks")
+
+
+def complete_task_view(request, index):
+    Task.objects.filter(id=index).update(completed=True)
+    return redirect("/tasks")
